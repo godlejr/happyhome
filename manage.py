f7a02c594d5a90439fffce18c62795715987f81c
sngwon <sngwon@gmail.com>
2016-07-07 16:27:38 +0900

    app 실행 파일 수정

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
-from flask_script import Manager
 from happyathome import create_app
 
 app = create_app()
-manager = Manager(app)
 
 if __name__ == '__main__':
+    from flask_script import Manager
+
+    manager = Manager(app)
     manager.run()
