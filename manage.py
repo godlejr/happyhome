34ffdafa1052b7ca1a06b259a4b284f67ab6453c
sngwon <sngwon@gmail.com>
2016-07-07 16:32:34 +0900

    띄어쓰기 수정

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from happyathome import create_app
 
-app = create_app(os.getenv('FLASK_CONFIG') or'default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
     from flask_script import Manager
