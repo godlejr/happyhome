4a911f079a387c41ac01cd9d6f7575c3cef2cc35
sngwon <sngwon@gmail.com>
2016-08-17 10:12:25 +0900

    사업자번호 유효성 체크 수정

--- happyathome/forms.py
+++ happyathome/forms.py
@@ -15,7 +15,7 @@ class BusinessCheck(object):
         key = [1, 3, 7, 1, 3, 7, 1, 3, 5]
         sum = 0
 
-        for i in range(0, 8):
+        for i in range(0, 9):
             sum += number[i] * key[i]
 
         sum += int((number[8] * 5) / 10)
