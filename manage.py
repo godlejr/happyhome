8ecfe240a77fbbb7f2cf0478765f0c211eef44fd
sngwon <sngwon@gmail.com>
2016-08-22 09:02:26 +0900

    갤러리 아이콘 변경

--- manage.py
+++ manage.py
@@ -1,23 +1,39 @@
+"""
+gunicorn 실행 모듈
+
+/var/app/inotone/bin/gunicorn manage:app -w 3
+"""
 import os
 
-from flask import url_for
+from flask import request
 from happyathome import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
+@app.url_defaults
+def hashed_url_for_static_file(endpoint, values):
+    if 'static' == endpoint or endpoint.endswith('.static'):
+        filename = values.get('filename')
         if filename:
-            file_path = os.path.join(app.root_path, endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+            if '.' in endpoint:  # has higher priority
+                blueprint = endpoint.rsplit('.', 1)[0]
+            else:
+                blueprint = request.blueprint  # can be None too
+
+            if blueprint:
+                static_folder = app.blueprints[blueprint].static_folder or app.static_folder
+            else:
+                static_folder = app.static_folder
+
+            param_name = 'q'
+            while param_name in values:
+                param_name = '_' + param_name
+            values[param_name] = static_file_hash(os.path.join(static_folder, filename))
+
+
+def static_file_hash(filename):
+    return int(os.stat(filename).st_mtime)
 
 
 if __name__ == '__main__':
