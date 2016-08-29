da43ff450713bb6448781f7b1ee43b2160a7ab18
Kimdongjoo <godlejr@gmail.com>
2016-08-29 10:45:48 +0900

    마우스 우클릭 드래그 금지, admin 수정중

--- happyathome/forms.py
+++ happyathome/forms.py
@@ -26,6 +26,16 @@ class BusinessCheck(object):
             raise ValidationError(self.message)
 
 
+class HomepageCheck(object):
+    def __init__(self, message=None):
+        if not message:
+            message = u''"http://"'를 반드시 입력해주세요.'
+        self.message = message
+
+    def __call__(self, form, field):
+        if field.data[0:7] != 'http://':
+            raise ValidationError(self.message)
+
 validators = {
     'email': [
         DataRequired(message='이메일을 입력해주세요'),
@@ -51,6 +61,9 @@ validators = {
         DataRequired(message='사업자번호를 입력하세요.'),
         Length(min=10, max=10, message='사업자번호는 10자리의 숫자입니다.'),
         BusinessCheck(message='유효한 사업자번호를 입력하세요.')
+    ],
+    'homepage': [
+        HomepageCheck(message=''"http://"'를 반드시 입력해주세요.')
     ]
 }
 
@@ -61,7 +74,7 @@ class ProfessionalUpdateForm(Form):
     address = StringField('주소찾기를 선택하세요.')
     sub_address = StringField('상세 주소를 입력하세요.')
     phone = StringField('ex) 010-0000-0000')
-    homepage = StringField('http://')
+    homepage = StringField(''"http://"'를 반드시 입력해주세요.', validators['homepage'])
 
 
 class PasswordUpdateForm(Form):
